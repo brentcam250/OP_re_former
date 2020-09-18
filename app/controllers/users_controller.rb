cdbb2c4af9998b9869f04a49396995329856ed3d
Brent Cameron <bjcameron@gmail.com>
2020-09-18 13:48:22 -0600

    create user#new view, add empty methods in controller for #new and #create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
-  
+
+  def new
+    # @user = User.new
+  end
+
+  def create
+    # @user = User.new(user_params)
+    # @user.save
+
+    # flash.notice = "User '#{@user.username}' saved!'"
+
+    # redirect_to user_path(@user)
+  end
+
 end
