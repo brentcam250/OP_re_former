b478a557593ff89d0b929def119e9fa546251d1e
Brent Cameron <bjcameron@gmail.com>
2020-09-21 15:30:47 -0600

    add edit functions, errors when bad input to edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      #success
+      flash.notice = "User '#{@user.username}' Updated!"
+      redirect_to new_user_path
+    else
+      flash.notice = "Errors '#{@user.errors.full_messages.to_sentence}' could not update"
+      redirect_to edit_user_path
+      #some errors
+
+    end
+  end 
+
   def create 
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
